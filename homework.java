0fd7731236258525ef2c86755b8d83c7f704ff56
yshyukk <104611426+yshyukk@users.noreply.github.com>
2022-05-20 09:11:50 +0900

    Add files via upload

---
+++ homework.java
@@ -0,0 +1,52 @@
+package backjunp2557;
+
+import java.util.Scanner;
+
+public class homework {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int menu = 0;
+		int balance = 0;
+		int input = 0;
+
+		do {
+			System.out.println("-------------------------");
+			System.out.println("1.예금 | 2. 출금 | 3. 잔고 | 4. 종료");
+			System.out.println("-------------------------");
+			System.out.println("선택>");
+
+			menu = scan.nextInt();
+
+			if (menu == 1 ) {
+				
+				input = scan.nextInt();
+				balance = input;
+
+				System.out.println("선택> "+ menu);
+				System.out.println("예금액>" + balance);
+
+			}else if (menu == 2) {
+				
+				input = scan.nextInt();
+				balance = balance - input;
+
+				System.out.println("선택> "+ menu);
+				System.out.println("출금액>" + balance);
+
+			}else if (menu == 3) {
+				System.out.println("선택> "+ menu);
+				System.out.println("잔고액>" + balance);
+
+			}else if (menu == 4) {
+				break;
+				
+			}
+		}while(true);
+				System.out.println("프로그램 종료");
+		
+	
+}
+}
+
